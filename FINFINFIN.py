ad3a9442a6a3287695efeddb448ee6d92e05690d
lololol77 <2310525@dtg.hs.kr>
2025-04-03 21:45:07 +0900

    Update FINFINFIN.py

--- FINFINFIN.py
+++ FINFINFIN.py
@@ -3,7 +3,7 @@ import streamlit as st
 import sqlite3
 
 # SQLite 데이터베이스 경로
-db_path = 'db1_corrected_final.sqlite'
+db_path = 'db1_abilities_complete_corrected.sqlite'
 
 # DB1 초기화 (SQLite 사용)
 def load_db1():
